705035807a214e29d2350c4f878245a1bce34e7e
HelibenModi <helimodi710@gmail.com>
2025-07-22 17:59:28 +0000

    User can rate

--- app/controllers/movie.php
+++ app/controllers/movie.php
@@ -20,5 +20,33 @@ class Movie extends Controller
     {
         $this->view('movie/search');
     }
+    public function search()
+    {
+        if (isset($_GET['title'])) {
+            $title = trim($_GET['title']);
+            $movie = $this->api->fetchMovie($title);
+
+            // Calculate average rating from DB
+            $stmt = $this->db->prepare("SELECT ROUND(AVG(rating), 1) AS avg_rating FROM ratings WHERE movie_title = ?");
+            $stmt->execute([$title]);
+            $avgRating = $stmt->fetch()['avg_rating'] ?? 'No ratings yet';
+
+            $this->view('movie/show', [
+                'movie' => $movie,
+                'avgRating' => $avgRating
+            ]);
+        } else {
+            header('Location: /movie');
+        }
+    }
+
+    public function rate()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'], $_POST['rating'])) {
+            $stmt = $this->db->prepare("INSERT INTO ratings (movie_title, rating) VALUES (?, ?)");
+            $stmt->execute([$_POST['title'], $_POST['rating']]);
+            header("Location: /movie/search?title=" . urlencode($_POST['title']));
+        }
+    }
 
 }
